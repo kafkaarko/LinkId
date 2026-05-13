921d8af992a6a74f0e8096341a28ab9ae1860e1f
kafkaarko <kafkaarko@gmail.com>
2026-05-13 11:07:50 +0700

    fix in appshell file '/.sidebar' to './SideBar'

--- fe/src/components/AppShell.jsx
+++ fe/src/components/AppShell.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import Sidebar from './Sidebar';
+import Sidebar from './SideBar';
 import { Outlet } from "react-router-dom";
 
 export default function AppShell({ children }) {
